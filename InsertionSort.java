ed16df96376741106d19434b23496db48973d1ca
Farros Mufid <farrosmufid.ofid@gmail.com>
2021-06-27 21:22:20 +0700

    first commit

---
+++ InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+
+    /*
+        Time: O(n^2)
+        Space: O(1)
+    */
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 6, 1, 3};
+
+        insertionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
