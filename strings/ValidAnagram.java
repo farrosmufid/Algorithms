aa3c0cf82763bfabf5ae8b075feccd4b14a2f384
Farros Mufid <farrosmufid.ofid@gmail.com>
2021-06-29 10:42:57 +0700

    Add ValidAnagram

--- strings/ValidAnagram.java
+++ strings/ValidAnagram.java
@@ -1,9 +1,75 @@
 package strings;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ValidAnagram {
 
+    /*
+        Follow up question: 
+        What if the inputs contain unicode characters? 
+        How would you adapt your solution to such case?
+
+        Solution: 
+        Use HashMap
+
+        Time: O(n)
+        Iterate string at length s
+
+        Space: O(n)
+        HashMap size grow with more varieties of characters
+    */
+
+    public boolean solution3(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1 );
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for (Character c: map.keySet()) {
+            if (map.get(c) != 0) { return false; }
+        }
+
+        return true;
+    }
+
+    /*
+        Solution 2: HashMap
+        Time: O(n)
+        Iterate string at length s and t.
+
+        Space: O(1)
+        - Although we do use extra space, the space complexity
+        is O(1) because the table's size stays constant no matter
+        how large n is.
+    */
+
+    public boolean solution2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int map[] = new int[26];
+        
+        for (char c: s.toCharArray()) {
+            map[c - 'a'] += 1;
+        }
+
+        for (char c: t.toCharArray()) {
+            map[c - 'a'] -= 1;
+            
+            if (map[c - 'a'] < 0) { // negative value means something is different
+                return false;
+            }
+        }
+        return true;
+    }
 
     /*
         Solution 1: Sorting
@@ -37,13 +103,11 @@ public class ValidAnagram {
     }
     public static void main(String[] args) {
         String s = "anagram";
-        String t = "nanagram";
+        String t = "aangrma";
 
         ValidAnagram va = new ValidAnagram();
 
-        boolean res = va.solution1(s, t);
+        boolean res = va.solution3(s, t);
         System.out.println(res);
-
-
     }
 }
